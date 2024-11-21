b40477edec64d900bd5666a16f4b22c2c3a499fb
Your Name <you@example.com>
2024-11-21 13:00:45 +0300

    8th update

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [], // Leave empty if using patterns
 
-    'allowed_origins_patterns' => ["*"],
-
+    'allowed_origins_patterns' => ['/^http:\/\/.+$/'],
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
