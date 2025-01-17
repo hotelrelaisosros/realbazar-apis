c09011f0669b0ccefed8265aa26d3603a1671d89
Your Name <you@example.com>
2025-01-17 14:24:35 +0300

    34 commit

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -16,7 +16,7 @@ class ProductImageResource extends JsonResource
     public function toArray($request)
     {
         return [
-            // 'id' => $this->id,
+            'id' => $this->id,
             'name' => $this->name,
             'image' => ImageHelper::formatImageUrl($this->image),
             'image_collection' => ImageHelper::formatImageCollection($this->image_collection),
